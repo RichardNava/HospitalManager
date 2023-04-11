6884bf9b51ddac6e976f9ff8c8638e9e0d943e91
RichardNava <richard_nava@hotmail.com>
2023-04-11 16:05:48 +0200

    Update sqlite

--- src/net/core/manager/HospitalManager.java
+++ src/net/core/manager/HospitalManager.java
@@ -1,9 +1,11 @@
 package net.core.manager;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,6 +23,7 @@ public class HospitalManager {
     public HospitalManager() {
         try (JdbcDAO dao = new JdbcDAO("jdbc:sqlite:hospital_manager.db")) {
             this.comunidades = new HashSet<>(dao.selectAllCA());
+            this.provincias = provinciasToStream().toList();
         } catch (SQLException ex) {
             Logger.getLogger(HospitalManager.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
@@ -54,4 +57,72 @@ public class HospitalManager {
                 .mapToInt(c -> numHospitalsCA(c.getProvinciaList()))
                 .sum();
     }
+    //imprimir el numero de hospitales por cada habitante de cada provincia
+    //Nº de camas en funcionamiento / Población * 100.000
+
+    public void numCamasFuncionamientoPromedio() {
+        provincias.stream().map(provincia -> numCamasHospitales(provincia) / provincia.getPoblacion() * 100000)
+                .forEach(System.out::println);
+    }
+
+    public Double numCamasHospitales(Provincia provincia) {
+        return provincia.getHospitalesList().stream().mapToDouble(h -> h.getTotalcamas()).sum();
+    }
+
+    public ComunidadAutonoma comunidadAutonomaById(int idComunidad) {
+        Optional<ComunidadAutonoma> op = comunidades.stream().filter(c -> c.getId() == idComunidad).findFirst();
+        //System.out.println(op.get());
+        return op.orElse(new ComunidadAutonoma());
+    }
+
+    public ComunidadAutonoma comunidadAutonomaById(String nombre) {
+        Optional<ComunidadAutonoma> op = comunidades.stream().filter(c -> c.getNombre().equalsIgnoreCase(nombre)).findFirst();
+        //System.out.println(op.get());
+        return op.orElse(new ComunidadAutonoma());
+    }
+
+    public Provincia provinciaById(int idProvincia) {
+        return idProvincia > 52 ? new Provincia() : provincias.get(idProvincia - 1);
+    }
+
+    public Provincia provinciaByName(String nombre) {
+        return provincias.stream().filter(p -> p.getNombre().equalsIgnoreCase(nombre)).findFirst().orElse(new Provincia());
+    }
+
+    // Imprimir el número de camas libres por provincia
+    public double numCamasLibresPorProvincia(int idProvincia) {
+        var provincia = provinciaById(idProvincia);
+        double numCamas = provincia.getHospitalesList().stream()
+                .mapToDouble(h -> h.getTotalcamas() - (h.getOcupadascovid19() + h.getOcupadasnocovid19())).sum();
+        System.out.println(provincia.getNombre() + " camas libres: " + numCamas);
+        return numCamas;
+    }
+
+    public double numCamasLibresPorProvincia(int idProvincia, LocalDate fechaInteres) {
+        var provincia = provinciaById(idProvincia);
+        double numCamas
+                = provincia.getHospitalesList().stream().filter(h -> fechaInteres.equals(LocalDate.parse(h.getFecha())))
+                        .mapToDouble(h -> h.getTotalcamas() - (h.getOcupadascovid19() + h.getOcupadasnocovid19())).sum();
+        System.out.println(provincia.getNombre() + " camas libres: " + numCamas);
+        return numCamas;
+    }
+
+    public double numCamasLibresPorProvincia(String nombreProv, LocalDate fechaInteres) {
+        var provincia = provinciaByName(nombreProv);
+        double numCamas
+                = provincia.getHospitalesList().stream().filter(h -> fechaInteres.equals(LocalDate.parse(h.getFecha())))
+                        .mapToDouble(h -> h.getTotalcamas() - (h.getOcupadascovid19() + h.getOcupadasnocovid19())).sum();
+        System.out.println(provincia.getNombre() + " camas libres: " + numCamas);
+        return numCamas;
+    }
+
+    public double mediaCamasOcupadaCovidPorProvincia(String nombreProv, LocalDate fechaInteres) {
+        var provincia = provinciaByName(nombreProv);
+        double numCamas
+                = provincia.getHospitalesList().stream().filter(h -> fechaInteres.equals(LocalDate.parse(h.getFecha())))
+                        .mapToDouble(h -> h.getOcupadascovid19() / h.getOcupadasnocovid19()).average().getAsDouble();
+        System.out.println(provincia.getNombre() + " camas libres: " + numCamas);
+        return numCamas;
+    }
+
 }
